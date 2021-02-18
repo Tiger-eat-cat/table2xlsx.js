53709ae2bcc773687ea94d858cf1ca8c10cab582
perry.shi <perry.shi@yunlsp.com>
2021-02-18 15:51:06 +0800

    test publish

---
+++ src/core.ts
@@ -0,0 +1,54 @@
+import ExcelJS from 'exceljs'
+
+export const createExcel = (selector: string | HTMLTableElement [] = 'table') => {
+    const workbook = new ExcelJS.Workbook()
+    const tableElements  = typeof selector === 'string' ? document.querySelectorAll(selector) : selector
+    const tables = Array.from(tableElements) as HTMLTableElement[]
+    tables.forEach((table, index) => {
+        const worksheet = workbook.addWorksheet(`Worksheet${index + 1}`)
+        const rows: HTMLTableRowElement [] = Array.from(table.rows)
+        const sheetHeight = rows.length
+        const sheetWidth = rows[sheetHeight - 1].cells.length
+        const generateRow = (): boolean [] => new Array(sheetWidth).fill(false)
+        const mergeLog: boolean[][] = new Array(sheetHeight).map(() => generateRow())
+        rows.forEach((row, rowIndex) => {
+            const y = rowIndex + 1 // 纵坐标
+            let x = 1 // 横坐标
+            const currentLineLog = mergeLog[rowIndex]
+            for (let i = 0; i < sheetWidth; i++) {
+                if (!currentLineLog[i]) {
+                    x = i + 1
+                    break
+                }
+            }
+            const cells: HTMLTableCellElement [] = Array.from(row.cells)
+            cells.forEach(cell => {
+                const { colSpan, rowSpan } = cell
+                const top = y // 开始行
+                const left = x // 开始列
+                const bottom = y + rowSpan - 1 // 结束行
+                const right = x + colSpan - 1 // 结束列
+                worksheet.mergeCells(top, left, bottom, right)
+                const sheetCell = worksheet.getCell(top, left)
+                sheetCell.value = cell.innerText
+                for (let i = top - 1; i < bottom; i ++) {
+                    for (let j = left - 1; j < right; j++) {
+                        mergeLog[i][j] = true
+                    }
+                }
+                x += colSpan
+            })
+        })
+    })
+    return {
+        export: async (filename: string = 'workbook') => {
+            const buffer: ArrayBuffer = await workbook.xlsx.writeBuffer()
+            const a = document.createElement('a')
+            const fileUrl = URL.createObjectURL(new Blob([buffer]))
+            a.href = fileUrl
+            a.download = `${filename}.xlsx`
+            a.click()
+            URL.revokeObjectURL(fileUrl)
+        }
+    }
+}
