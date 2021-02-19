da706a1d4919469dfc970b380fdb793cb04f7846
perry.shi <perry.shi@yunlsp.com>
2021-02-19 10:39:04 +0800

    fix set font style error

--- src/processor.ts
+++ src/processor.ts
@@ -7,11 +7,15 @@ export const fontProcessor = (cell: HTMLTableCellElement, sheetCell: Cell): void
     const fontSize: string = style.fontSize
     const textAlign: string = style.textAlign
     const color: string = rgbToArgb(style.color)
-    sheetCell.font.size = parseInt(fontSize)
-    sheetCell.font.name = style.fontFamily
-    sheetCell.font.color = { argb: color }
+    sheetCell.font = {
+        size: parseInt(fontSize),
+        name: style.fontFamily,
+        color: { argb: color },
+    }
     if (TEXT_ALIGN.some(item => item === textAlign)) {
-        // @ts-ignore
-        sheetCell.alignment.horizontal = textAlign
+        sheetCell.alignment = {
+            // @ts-ignore
+            horizontal: textAlign,
+        }
     }
 }
